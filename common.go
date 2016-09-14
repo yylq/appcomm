90af4df298e0fbafb721714f67faad411190e796
yylq <51985725@qq.com>
2016-09-14 10:55:29 +0800

    mode for some

--- common.go
+++ common.go
@@ -1,8 +1,13 @@
-package common
+package appcomm
 
 import (
-	"godsee/log"
+	"fmt"
+	"io/ioutil"
 	"os"
+
+	"github.com/yylq/log"
+
+	"strconv"
 
 	"github.com/yylq/config"
 )
@@ -45,4 +50,26 @@ func GetHostName() string {
 
 	}
 	return id
+}
+func PahtExist(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func GetPid(file string) (int, error) {
+	buff, err := ioutil.ReadFile(file)
+	if err != nil {
+		return -1, err
+	}
+	return strconv.Atoi(string(buff))
+}
+func WritePid(file string, pid int) error {
+	s := fmt.Sprintf("%d", pid)
+	return ioutil.WriteFile(file, []byte(s), os.ModePerm)
 }
--- mq.go
+++ mq.go
@@ -1,4 +1,4 @@
-package common
+package appcomm
 
 import (
 	"fmt"
